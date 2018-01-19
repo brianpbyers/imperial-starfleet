b2de3671cbe7d407fb750274a021fbb61369db19
anhonestmouse <anhonestmouse@gmail.com>
2018-01-18 17:32:13 -0700

    finished

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { InternalDefensesModule } from './internal-defenses/internal-defenses.module';
+import { ExternalDefensesModule } from './external-defenses/external-defenses.module';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ExternalDefensesModule,
+    InternalDefensesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
